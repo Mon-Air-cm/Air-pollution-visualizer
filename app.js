92c424469ae0ce56ce5aa099d6ef827f71e3d9ac
Bryan Wu <bryanwu@berkeley.edu>
2020-02-15 08:38:57 -0800

    add dummy data

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ require([
     "esri/widgets/BasemapGallery",
     "esri/layers/FeatureLayer",
   ], function(Map, MapView, BasemapToggle, BasemapGallery, FeatureLayer) {
-
   var map = new Map({
     basemap: "topo-vector"
   });
@@ -38,5 +37,79 @@ require([
         });
   map.add(trailsLayer, 0);
   map.add(parksLayer, 0);
+  var monitorRenderer = {
+        type: "simple",
+        symbol: {
+          type: "picture-marker",
+          url: "something something url",//a url linking to the arcGIS hosting the data and the information
+          width: "18px",
+          height: "18px"
+        }
+      }
+  var trailheadsLabel = {
+    symbole: {
+      test: "text",
+      color: "#FFFFFF",
+      haloColor: "5E8D74",
+      haloSize: "2px",
+    },
+    font: {
+            size: "12px",
+            family: "Noto Sans",
+            style: "italic",
+            weight: "normal"
+          },
+    labelPlacement: "above-center",
+    labelExpressionInfo: {
+      expression: "$feature.TRL_NAME"
+    }
+  };
+  var trailheads = new FeatureLayer({
+    url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0",
+    renderer: trailheadsRenderer,
+    labelingInfo: [trailheadsLabel]
+  })
+  map.add(trailheads);
+  var trailsRenderer = {
+    type: "simple",
+    symbol: {
+          color: "#BA55D3",
+          type: "simple-line",
+          style: "solid"
+        },
+    visualVariables: [
+          {
+            type: "size",
+            field: "ELEV_GAIN",
+            minDataValue: 0,
+            maxDataValue: 2300,
+            minSize: "3px",
+            maxSize: "7px"
+          }
+        ],
+  }
+  var trails = new FeatureLayer({
+    url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails/FeatureServer/0",
+    renderer: trailsRenderer,
+    opacity: 0.5,
+  });
+  map.add(trails, 0);
+  var bikeTrailsRenderer = {
+  type: "simple",
+  symbol: {
+    type: "simple-line",
+    style: "short-dot",
+    color: "#FF91FF",
+    width: "6px"
+    }
+  };
+  var bikeTrails = new FeatureLayer({
+        url:
+          "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails/FeatureServer/0",
+        renderer: bikeTrailsRenderer,
+        definitionExpression: "USE_BIKE = 'YES'"
+      });
+
+      map.add(bikeTrails, 1);
   view.ui.add(basemapGallery, "bottom-right");
 });
